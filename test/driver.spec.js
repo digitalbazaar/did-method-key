fe91c18b9bdb4f1a74debc6bf091b7bc0a285c52
Dmitri Zagidulin <dzagidulin@gmail.com>
2021-04-09 11:37:34 -0400

    Throw error if publicMethodFor is not found.

--- test/driver.spec.js
+++ test/driver.spec.js
@@ -123,15 +123,23 @@ describe('did:key method driver', () => {
           'zB12NYF8RrR3h41TDCTJojY59usg3mbtbjnFs7Eud1Y6u');
     });
 
-    it('should return undefined if key is not found for purpose', async () => {
+    it('should throw error if key is not found for purpose', async () => {
       const did = 'did:key:z6MkpTHR8VNsBxYAAWHut2Geadd9jSwuBV8xRoAnwWsdvktH';
       // First, get the did document
       const didDocument = await didKeyDriver.get({did});
-      // Then publicMethodFor can be used to fetch key data
-      const result = didKeyDriver.publicMethodFor({
-        didDocument, purpose: 'invalidPurpose'
-      });
-      expect(result).to.be.undefined;
+
+      let error;
+      try {
+        didKeyDriver.publicMethodFor({
+          didDocument, purpose: 'invalidPurpose'
+        });
+      } catch(e) {
+        error = e;
+      }
+
+      expect(error).to.be.exist;
+      expect(error.message).to
+        .match(/No verification method found for purpose/);
     });
   });
 
